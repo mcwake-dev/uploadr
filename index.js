4a96b1d73294f1ff2bbdf63a3f8034e0887a147d
Matthew C Wake <mcwake-dev@outlook.com>
2023-02-08 21:33:09 +0000

    Added static file hosting for uploaded files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const path = require("path");
 const cors = require("@fastify/cors");
 const fastify = require("fastify")({ logger: true });
 const multipart = require("@fastify/multipart");
 
 fastify.register(cors, {});
 fastify.register(multipart, {});
+fastify.register(require("@fastify/static"), {
+    root: path.join(__dirname, "quarantine"),
+    prefix: "/public/"
+})
 fastify.register(require("./plugins/av.plugin"));
 fastify.register(require("./routes/upload.routes"));
 
